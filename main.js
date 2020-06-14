e5895748bf298fb241be08684e6ea6130f3abd84
Josue G <josue.ggh@gmail.com>
2020-06-13 21:52:47 -0500

    Adding the ScatterplotLayer

--- main.js
+++ main.js
@@ -6,11 +6,38 @@ const {ScatterplotLayer, GoogleMapsOverlay} = deck;
 
 let GMAP , DECKGL_OVERLAY;
 
-
-
-function updateMap(country) {
-  console.log(this.textContent);
+async function updateMap() {
+  const $activeElement = document.querySelector(".is-active");
+  if ($activeElement) {
+    $activeElement.classList.remove('is-active')
+  }
+  this.classList.add('is-active')
+  let country = this.textContent.replace(/ /g,'').toLowerCase();
+  let layer = await getLayer(country);
+  DECKGL_OVERLAY.setProps({layers: [layer]});
 }
+
+async function getLayer(country = 'austria'){
+    const request_country =  await fetch(`./data/${country}.json`);
+    const data_country = await request_country.json();
+
+    return await new ScatterplotLayer({
+        id : 'trees',
+        data: data_country,
+        opacity: 0.8,
+        stroked: true,
+        filled: true,
+        radiusScale: 6,
+        radiusMinPixels: 1,
+        radiusMaxPixels: 100,
+        lineWidthMinPixels: 1,
+        getPosition: d=> [d.lng, d.lat],
+        getRadius : d => 50,
+        getFillColor: d=> [0,155,115],
+        getLineColor: d=> [30,30,30]
+    });
+}
+
 function renderButtonElement(country) {
   const element = document.createElement('button')
   element.textContent = country
@@ -23,11 +50,15 @@ async function renderButtons() {
   countries.forEach(renderButtonElement);
 }
 
-function initMap(){
+async function initMap(){
     GMAP = new google.maps.Map($map , {
         zoom : 4,
         center: EUROPE_CENTER
     });
+
+    DECKGL_OVERLAY = new GoogleMapsOverlay();
+    DECKGL_OVERLAY.setMap(GMAP);
+    DECKGL_OVERLAY.setProps({ layers: [ await getLayer()] })
     renderButtons()
 }
 
